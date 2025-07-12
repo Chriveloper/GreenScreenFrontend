abd0e47ce60b3b82aa43887d7271d88244e7f401
Chriveloper <111074541+Chriveloper@users.noreply.github.com>
2025-07-12 15:38:32 +0200

    Implement login and password reset functionality; add loading states and error handling

--- plugins/supabase.client.ts
+++ plugins/supabase.client.ts
@@ -1,14 +1,25 @@
 import { createClient } from '@supabase/supabase-js';
 import { defineNuxtPlugin, useRuntimeConfig } from '#app';
 
-export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig().public;
-  const supabaseUrl = config.supabaseUrl as string;
-  const supabaseKey = config.supabaseKey as string;
-  const supabase = createClient(supabaseUrl, supabaseKey);
-  return {
-    provide: {
-      supabase
+export default defineNuxtPlugin({
+  name: 'supabase',
+  enforce: 'pre', // Makes this plugin run before other plugins
+  setup() {
+    const config = useRuntimeConfig().public;
+    const supabaseUrl = config.supabaseUrl as string;
+    const supabaseKey = config.supabaseKey as string;
+    
+    if (!supabaseUrl || !supabaseKey) {
+      console.error('Supabase URL or Key is missing from environment variables');
     }
-  };
+    
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    console.log('Supabase client initialized');
+    
+    return {
+      provide: {
+        supabase
+      }
+    };
+  }
 });
